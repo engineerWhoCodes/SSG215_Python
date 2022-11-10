d8f9d34f88cf38b4d2db02438ca2a50bd9e041ff
engineerWhoCodes <www.gconglomerates@gmail.com>
2022-11-10 14:09:43 +0100

    commutative union

---
+++ Sets/union.py
@@ -0,0 +1,5 @@
+#Checking if the union of the sets are commutative
+ade = {'singlet', 'shorts', 'teeshirt', 'slippers', 'headphones', 'wristwatch', 'boxers',}
+imman = {'longsleeve', 'trousers', 'slippers', 'boxers'}
+print('Imman union Tobi is:', imman.union(ade))
+print('Ade union Tobi is: ', ade.union(imman))
