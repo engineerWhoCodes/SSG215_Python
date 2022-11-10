f8d780330b9d31681a21ec71d49658628464618c
engineerWhoCodes <www.gconglomerates@gmail.com>
2022-11-10 14:01:59 +0100

    coummutative

---
+++ Sets/intersect1.py
@@ -0,0 +1,5 @@
+#Demonstrating that for any two sets S and T, the intersection is commutative
+ade = {'singlet', 'shorts', 'teeshirt', 'slippers', 'headphones', 'wristwatch', 'boxers',}
+imman = {'longsleeve', 'trousers', 'slippers', 'boxers'}
+print(ade.intersection(imman))
+print(imman.intersection(ade))
