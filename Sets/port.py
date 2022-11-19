316caef8e98c8c92f154529c9c7e5a814d432434
engineerWhoCodes <www.gconglomerates@gmail.com>
2022-11-19 19:27:11 +0100

    Exploring portion module

---
+++ Sets/port.py
@@ -0,0 +1,17 @@
+#The portion module provides data struture and operations for intervals in python
+import portion as por
+from matplotlib_venn import venn2, venn2_circles
+import matplotlib.pyplot as plt
+compSet_1 = {i for i in range(0, 21)}
+compSet_2 = {iota for iota in range(15, 27)}
+porSet_1 = por.closed(0, 20)
+porSet_2 = por.openclosed(4, 27)
+porSet_3 = por.closed(13, 35)
+answer = 3.5 in porSet_2
+test = 18 in compSet_2.intersection(compSet_1)
+venn2([compSet_1, compSet_2],
+set_labels = ('First set', 'Second set'),
+set_colors = ('purple', 'yellow'), alpha = 0.8)
+venn2_circles([compSet_1, compSet_2])
+plt.title('Sample code')
+plt.show()
