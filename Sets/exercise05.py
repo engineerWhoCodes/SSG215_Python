b3069a3319359922848134ae97a6e9782d811cf9
engineerWhoCodes <www.gconglomerates@gmail.com>
2022-11-19 18:22:11 +0100

    Modified

--- Sets/exercise05.py
+++ Sets/exercise05.py
@@ -1,5 +1,5 @@
 #Checking the availability of items in a set against a particular set and then checking for avialability
-authorsLib = {'Fagunwa', 'Achebe', 'Shakespeare', 'Clark', 'Saro Wiwa', 'Ngugi', 'Nwankwo', 'Austen', 'Eliot', 'Hardy', 'Bunyan', 'Tutuola', 'Delano', 'Ulasi', 'Odunjo', }
+authorsLib = {'Fagunwa', 'Achebe', 'Shakespeare', 'Clark', 'Saro Wiwa', 'Ngugi', 'Nwankwo', 'Austen', 'Eliot', 'Hardy', 'Bunyan', 'Tutuola', 'Delano', 'Ulasi', 'Odunjo', 'Yinka', 'Tho' }
 myList = {'Olabode', 'Yuppie', 'Adamu', 'Tho', 'Adichie', 'Achebe', 'Soyinka', 'Nwanko', 'Bronte', 'Rakumi', 'Yinka', 'Loto', 'Mu',}
 myDict = {}
 for auth in myList:
@@ -7,4 +7,4 @@ for auth in myList:
         myDict[auth] = 'Available'
     else:
         myDict[auth] = 'Not available'
-print('The availability list: ', myDict)
+    print('The availability list: ', myDict)
