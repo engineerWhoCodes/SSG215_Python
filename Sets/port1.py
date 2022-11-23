b346a46cd81db0a8734073a4550dc628ceb8f9a3
engineerWhoCodes <www.gconglomerates@gmail.com>
2022-11-23 08:35:14 +0100

    Added pass statement

--- Sets/port1.py
+++ Sets/port1.py
@@ -1,7 +1,10 @@
+
+#Finding unions and intersections of intervals.
 import portion as por
 porSet_01 = por.closed(0, 20)
 porSet_02 = por.openclosed(4, 27)
 porSet_03 = por.closed(13, 35)
 uniInt = porSet_01.union(porSet_02)
 intSet = porSet_03.intersection(porSet_01)
-print(intSet)
+
+pass
