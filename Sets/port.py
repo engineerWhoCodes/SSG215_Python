33bd3f6ca9e8d66c7a00c7b2807bd18994a2b2dd
engineerWhoCodes <www.gconglomerates@gmail.com>
2022-11-23 06:16:57 +0100

    Detailed explanation

--- Sets/port.py
+++ Sets/port.py
@@ -2,8 +2,8 @@
 import portion as por
 from matplotlib_venn import venn2, venn2_circles
 import matplotlib.pyplot as plt
-compSet_1 = {i for i in range(0, 21)}
-compSet_2 = {iota for iota in range(15, 27)}
+compSet_1 = {i for i in range(0, 21)} #list comprehension
+compSet_2 = {iota for iota in range(15, 27)} #list comprehension
 porSet_1 = por.closed(0, 20)
 porSet_2 = por.openclosed(4, 27)
 porSet_3 = por.closed(13, 35)
@@ -13,5 +13,7 @@ venn2([compSet_1, compSet_2],
 set_labels = ('First set', 'Second set'),
 set_colors = ('purple', 'yellow'), alpha = 0.8)
 venn2_circles([compSet_1, compSet_2])
-plt.title('Sample code')
-plt.show()
+#plt.title('Sample code')
+#plt.show()
+#The above lines of codes is to prove that sets can involve discrete elements, comprehensions as well intervals.
+print(test)
