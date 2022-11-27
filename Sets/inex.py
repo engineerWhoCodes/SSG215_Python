a589fe2c3c8db31ad6243c93cac57449aab868d3
engineerWhoCodes <www.gconglomerates@gmail.com>
2022-11-27 15:30:36 +0100

    in and ex joining

---
+++ Sets/inex.py
@@ -0,0 +1,11 @@
+#Showing that implicit and explicit line joining produces the same result
+#Implicit line continuations(breaking the commas )
+olu = {'eba', 'okro', 'beans', 
+'boli', 'rice', 'stew'}
+
+#Explicit line continuations(backlash to create a forced line)
+chike = {'rice', 'ugwu', 'akpu', \
+    'beans', 'plaintains' \
+        'yams', 'agazi'}
+print(olu)
+print(chike)
